a10f512d8b0609975e48fe38079553caf72ef221
Kyle Safran <kylesafran@Kyles-MacBook-Air.local>
2018-09-15 23:08:42 -0400

    redirect to poll

--- api/routes.py
+++ api/routes.py
@@ -1,5 +1,5 @@
 """routes"""
-from flask import Blueprint, current_app, render_template, request
+from flask import Blueprint, current_app, render_template, request, redirect
 from .model import mehbooyeh, db
 from .forms import MBYForm
 
@@ -9,26 +9,18 @@ BLUEPRINT = Blueprint("BLUEPRINT", __name__)
 @BLUEPRINT.route("/", methods=['GET', 'POST'])
 def add_boop():
     form = MBYForm(request.form)
-    import pdb; pdb.set_trace()
-
     if request.method == 'POST' and form.validate():
-        import pdb; pdb.set_trace()
-
         reg = mehbooyeh(form.name.data, form.mby.data)
         db.session.add(reg)
         db.session.commit()
-        return render_template('success.html')
+        meh_id = reg.id
+        return redirect('/meh/' + str(meh_id))
     return render_template('index.html', form = form)
 
+@BLUEPRINT.route('/meh/<meh_id>', methods=['GET','POST'])
+def meh_id(meh_id):
 
-@BLUEPRINT.route('/reg', methods=['POST'])
-def reg():
-    form = MBYForm(request.form)
-    if request.method == 'POST':
-        name = request.form['name']
-        item = request.form['item']
-        reg = mehbooyeh(name, item)
-        db.session.add(reg)
-        db.session.commit()
-        return render_template('success.html')
-    return render_template('index.html')
+    meh = db.session.query(mehbooyeh).filter(mehbooyeh.id == meh_id).first()
+    return render_template('mehbooyeh.html',
+                           name=meh.name, item=meh.item,
+                           mehs=meh.mehs, boos=meh.boos, yehs=meh.yehs)
