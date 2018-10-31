bc9358d25f480bfef62cbbd1f225dd773c43d9d3
Nick Oki <nick.oki@live.com>
2018-10-31 14:38:16 -0400

    Init scss

--- api/__init__.py
+++ api/__init__.py
@@ -1,5 +1,6 @@
 """application factory"""
 from flask import Flask
+from flask_assets import Environment, Bundle
 from .routes import BLUEPRINT
 from .config import database_uri, secret_key
 from .model import db
@@ -8,9 +9,17 @@ db = db
 def create_app():
     """makes the application"""
     app = Flask(__name__)
+
     app.config['SQLALCHEMY_DATABASE_URI'] = database_uri
     app.config['SECRET_KEY'] = secret_key
+
     with app.app_context():
         db.init_app(app)
-    app.register_blueprint(BLUEPRINT)
+        app.register_blueprint(BLUEPRINT)
+
+    assets = Environment(app)
+    assets.url = app.static_url_path
+    scss = Bundle('css/styles.scss', filters='pyscss', output='all.css')
+    assets.register('scss_all', scss)
+
     return app
